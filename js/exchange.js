5a4698b87f635ac069d81e8e9b43f23b8412ac58
123356 <1017510820@qq.com>
2018-09-10 10:14:38 +0800

    1

--- js/exchange.js
+++ js/exchange.js
@@ -1,7 +1,14 @@
 (function (){
 
     window.addEventListener("load", function(){
-        var data = [100,200,0,700,230,12,58,68,35,99,11,22,5,88,99,66,58,458,457,52,35,25,158,15,12,700,230,700,230,12,58,68,35,99,11,22,5,88,99,66,58,458,457,52,35,25,158,15,12,12,58,68,35,99,11,22,5,88,99,66,58,458,457,52,35,25,158,15,12,15,88,35,21,25,258,33,245,698,85,69,36,333];
+        var data = [
+            [{x:10,y:200},{x:11 ,y:15 },{x: 15,y:65 },{x:20 ,y:75 },{x:30 ,y: 50},{x: 41,y:85 }],
+            [{x: 15,y:65 },{x:20 ,y:75 },{x:30 ,y: 50},{x: 41,y:85 },{x:65 ,y:75 },{x: 66,y:14 }],
+            [{x:20 ,y:75 },{x:30 ,y: 50},{x: 41,y:85 },{x:65 ,y:75 },{x: 66,y:14 },{x: 70,y:125 },{x:88 ,y: 99}],
+            [{x: 41,y:85 },{x:65 ,y:75 },{x: 66,y:14 },{x: 70,y:125 },{x:88 ,y: 99}],
+            [{x:65 ,y:75 },{x: 66,y:14 },{x: 70,y:125 },{x:88 ,y: 99},{x:99 ,y: 88},{x:100 ,y:200 }],
+            [{x:10,y:200},{x:11 ,y:15 },{x: 15,y:65 },{x:20 ,y:75 },{x:30 ,y: 50}, {x: 41,y:85 },{x:65 ,y:75 },{x: 66,y:14 },{x: 70,y:125 },{x:88 ,y: 99},{x:99 ,y: 88},{x:100 ,y:200 }]
+            ];
         // 获取上下文
         var a_canvas = document.getElementById('can');
         var context = a_canvas.getContext("2d");
@@ -12,424 +19,71 @@
         context.fillStyle = gradient;
         context.fillRect(0,0,a_canvas.width,a_canvas.height);
         // 描绘边框
-        var grid_cols = data.length + 1;
-        var grid_rows = 4;
-        var cell_height = a_canvas.height / grid_rows;
-        var cell_width = a_canvas.width / grid_cols;
+        var cell_height = a_canvas.height / 4;
+        var cell_width = a_canvas.width / 3;
         context.lineWidth = 1;
         context.strokeStyle = "#a0a0a0";
         // 结束边框描绘
         context.beginPath();
         // 准备画横线
-        for (var col = 0; col <= grid_cols; col++) {
+        for (var col = 0; col <3; col++) {
             var x = col * cell_width;
+            context.setLineDash([5])
             context.moveTo(x,0);
             context.lineTo(x,a_canvas.height);
+            context.strokeStyle = "#ededed";
+            context.stroke();
         }
         // 准备画竖线
-        for(var row = 0; row <= grid_rows; row++){
+        for(var row = 0; row < 4; row++){
             var y = row * cell_height;
+            context.beginPath()
             context.moveTo(0,y);
             context.lineTo(a_canvas.width, y);
+            context.strokeStyle = "#ededed";
+            context.stroke();
         }
-        //划横线
-        context.moveTo(0,a_canvas.height/2);
-        context.lineTo(a_canvas.width,a_canvas.height/2);
-        //画竖线
-        context.moveTo(0,0);
-        context.lineTo(0,a_canvas.height);
-        context.lineWidth = 1;
-        context.strokeStyle = "#ededed";
-        context.stroke();
-        var max_v =0;
-        for(var i = 0; i<data.length; i++){
-            var d=0;
-            if(data[i]<0)
-            {dd=d-data[i];
-            }
-            else{d=data[i];};
-            if (d > max_v) { max_v =d};
-        }
-        max_vmax_v = max_v * 1.1;
         // 将数据换算为坐标
         var points = [];
-        for( var i=0; i < data.length; i++){
-            var v= data[i];
-            var px = cell_width *　(i +1);
-            var py = a_canvas.height/2 - a_canvas.height*(v / max_v)/2;
-            points.push({"x":px,"y":py});
-        }
-        // 绘制折现
-        context.beginPath();
-        context.moveTo(points[0].x, points[0].y);
-        for(var i= 1; i< points.length; i++){
-            context.lineTo(points[i].x,points[i].y);
-        }
-        context.lineWidth = 2;
-        context.strokeStyle = "#5C6287";
-        context.stroke();
+        var v= data[0];
+            v.forEach(function (item,i) {
+                var px = item.x;
+                var py = a_canvas.height-(item.y);
+                points.push({"x":px,"y":py});
+            })
+            context.beginPath();
+            context.setLineDash([0])
+            context.moveTo(points[0].x, points[0].y);
+            for(var i= 1; i< points.length; i++){
+                context.lineTo(points[i].x,points[i].y);
+            }
+            context.lineWidth = 1.2;
+            context.strokeStyle = "#535880";
+            context.stroke();
+        $(".e_containerRight_item").each(function (index) {
+            $(this).hover(function () {
+                ($(".canvas")).appendTo($(this))
+                var point = [];
+                var o= data[index];
+                o.forEach(function (item,i) {
+                    var px = item.x;
+                    var py = a_canvas.height-(item.y);
+                    point.push({"x":px,"y":py});
+                })
+                context.beginPath();
+                context.setLineDash([0])
+                context.moveTo(point[0].x, point[0].y);
+                for(var i= 1; i< point.length; i++){
+                    context.lineTo(point[i].x,point[i].y);
+                }
+                context.lineWidth = 1.2;
+                context.strokeStyle = "#535880";
+                context.stroke();
+            })
+        })
     },false);
 })();
-(function (){
 
-    window.addEventListener("load", function(){
-        var data = [100,200,0,700,230,12,58,68,35,99,11,22,5,88,99,66,58,458,457,52,35,25,158,15,12,700,230,700,230,12,58,68,35,99,11,22,5,88,99,66,58,458,457,52,35,25,158,15,12,12,58,68,35,99,11,22,5,88,99,66,58,458,457,52,35,25,158,15,12,15,88,35,21,25,258,33,245,698,85,69,36,333];
-        // 获取上下文
-        var a_canvas = document.getElementById('can1');
-        var context = a_canvas.getContext("2d");
-        // 绘制背景
-        var gradient = context.createLinearGradient(0,0,300,135);
-        gradient.addColorStop(0,"#ffffff");
-        gradient.addColorStop(1,"#F7F7F7");
-        context.fillStyle = gradient;
-        context.fillRect(0,0,a_canvas.width,a_canvas.height);
-        // 描绘边框
-        var grid_cols = data.length + 1;
-        var grid_rows = 4;
-        var cell_height = a_canvas.height / grid_rows;
-        var cell_width = a_canvas.width / grid_cols;
-        context.lineWidth = 1;
-        context.strokeStyle = "#a0a0a0";
-        // 结束边框描绘
-        context.beginPath();
-        // 准备画横线
-        for (var col = 0; col <= grid_cols; col++) {
-            var x = col * cell_width;
-            context.moveTo(x,0);
-            context.lineTo(x,a_canvas.height);
-        }
-        // 准备画竖线
-        for(var row = 0; row <= grid_rows; row++){
-            var y = row * cell_height;
-            context.moveTo(0,y);
-            context.lineTo(a_canvas.width, y);
-        }
-        //划横线
-        context.moveTo(0,a_canvas.height/2);
-        context.lineTo(a_canvas.width,a_canvas.height/2);
-        //画竖线
-        context.moveTo(0,0);
-        context.lineTo(0,a_canvas.height);
-        context.lineWidth = 1;
-        context.strokeStyle = "#ededed";
-        context.stroke();
-        var max_v =0;
-        for(var i = 0; i<data.length; i++){
-            var d=0;
-            if(data[i]<0)
-            {dd=d-data[i];
-            }
-            else{d=data[i];};
-            if (d > max_v) { max_v =d};
-        }
-        max_vmax_v = max_v * 1.1;
-        // 将数据换算为坐标
-        var points = [];
-        for( var i=0; i < data.length; i++){
-            var v= data[i];
-            var px = cell_width *　(i +1);
-            var py = a_canvas.height/2 - a_canvas.height*(v / max_v)/2;
-            points.push({"x":px,"y":py});
-        }
-        // 绘制折现
-        context.beginPath();
-        context.moveTo(points[0].x, points[0].y);
-        for(var i= 1; i< points.length; i++){
-            context.lineTo(points[i].x,points[i].y);
-        }
-        context.lineWidth = 2;
-        context.strokeStyle = "#5C6287";
-        context.stroke();
-    },false);
-})();
-(function (){
-
-    window.addEventListener("load", function(){
-        var data = [100,200,0,700,230,12,58,68,35,99,11,22,5,88,99,66,58,458,457,52,35,25,158,15,12,700,230,700,230,12,58,68,35,99,11,22,5,88,99,66,58,458,457,52,35,25,158,15,12,12,58,68,35,99,11,22,5,88,99,66,58,458,457,52,35,25,158,15,12,15,88,35,21,25,258,33,245,698,85,69,36,333];
-        // 获取上下文
-        var a_canvas = document.getElementById('can2');
-        var context = a_canvas.getContext("2d");
-        // 绘制背景
-        var gradient = context.createLinearGradient(0,0,300,135);
-        gradient.addColorStop(0,"#ffffff");
-        gradient.addColorStop(1,"#F7F7F7");
-        context.fillStyle = gradient;
-        context.fillRect(0,0,a_canvas.width,a_canvas.height);
-        // 描绘边框
-        var grid_cols = data.length + 1;
-        var grid_rows = 4;
-        var cell_height = a_canvas.height / grid_rows;
-        var cell_width = a_canvas.width / grid_cols;
-        context.lineWidth = 1;
-        context.strokeStyle = "#a0a0a0";
-        // 结束边框描绘
-        context.beginPath();
-        // 准备画横线
-        for (var col = 0; col <= grid_cols; col++) {
-            var x = col * cell_width;
-            context.moveTo(x,0);
-            context.lineTo(x,a_canvas.height);
-        }
-        // 准备画竖线
-        for(var row = 0; row <= grid_rows; row++){
-            var y = row * cell_height;
-            context.moveTo(0,y);
-            context.lineTo(a_canvas.width, y);
-        }
-        //划横线
-        context.moveTo(0,a_canvas.height/2);
-        context.lineTo(a_canvas.width,a_canvas.height/2);
-        //画竖线
-        context.moveTo(0,0);
-        context.lineTo(0,a_canvas.height);
-        context.lineWidth = 1;
-        context.strokeStyle = "#ededed";
-        context.stroke();
-        var max_v =0;
-        for(var i = 0; i<data.length; i++){
-            var d=0;
-            if(data[i]<0)
-            {dd=d-data[i];
-            }
-            else{d=data[i];};
-            if (d > max_v) { max_v =d};
-        }
-        max_vmax_v = max_v * 1.1;
-        // 将数据换算为坐标
-        var points = [];
-        for( var i=0; i < data.length; i++){
-            var v= data[i];
-            var px = cell_width *　(i +1);
-            var py = a_canvas.height/2 - a_canvas.height*(v / max_v)/2;
-            points.push({"x":px,"y":py});
-        }
-        // 绘制折现
-        context.beginPath();
-        context.moveTo(points[0].x, points[0].y);
-        for(var i= 1; i< points.length; i++){
-            context.lineTo(points[i].x,points[i].y);
-        }
-        context.lineWidth = 2;
-        context.strokeStyle = "#5C6287";
-        context.stroke();
-    },false);
-})();
-(function (){
-
-    window.addEventListener("load", function(){
-        var data = [100,200,0,700,230,12,58,68,35,99,11,22,5,88,99,66,58,458,457,52,35,25,158,15,12,700,230,700,230,12,58,68,35,99,11,22,5,88,99,66,58,458,457,52,35,25,158,15,12,12,58,68,35,99,11,22,5,88,99,66,58,458,457,52,35,25,158,15,12,15,88,35,21,25,258,33,245,698,85,69,36,333];
-        // 获取上下文
-        var a_canvas = document.getElementById('can3');
-        var context = a_canvas.getContext("2d");
-        // 绘制背景
-        var gradient = context.createLinearGradient(0,0,300,135);
-        gradient.addColorStop(0,"#ffffff");
-        gradient.addColorStop(1,"#F7F7F7");
-        context.fillStyle = gradient;
-        context.fillRect(0,0,a_canvas.width,a_canvas.height);
-        // 描绘边框
-        var grid_cols = data.length + 1;
-        var grid_rows = 4;
-        var cell_height = a_canvas.height / grid_rows;
-        var cell_width = a_canvas.width / grid_cols;
-        context.lineWidth = 1;
-        context.strokeStyle = "#a0a0a0";
-        // 结束边框描绘
-        context.beginPath();
-        // 准备画横线
-        for (var col = 0; col <= grid_cols; col++) {
-            var x = col * cell_width;
-            context.moveTo(x,0);
-            context.lineTo(x,a_canvas.height);
-        }
-        // 准备画竖线
-        for(var row = 0; row <= grid_rows; row++){
-            var y = row * cell_height;
-            context.moveTo(0,y);
-            context.lineTo(a_canvas.width, y);
-        }
-        //划横线
-        context.moveTo(0,a_canvas.height/2);
-        context.lineTo(a_canvas.width,a_canvas.height/2);
-        //画竖线
-        context.moveTo(0,0);
-        context.lineTo(0,a_canvas.height);
-        context.lineWidth = 1;
-        context.strokeStyle = "#ededed";
-        context.stroke();
-        var max_v =0;
-        for(var i = 0; i<data.length; i++){
-            var d=0;
-            if(data[i]<0)
-            {dd=d-data[i];
-            }
-            else{d=data[i];};
-            if (d > max_v) { max_v =d};
-        }
-        max_vmax_v = max_v * 1.1;
-        // 将数据换算为坐标
-        var points = [];
-        for( var i=0; i < data.length; i++){
-            var v= data[i];
-            var px = cell_width *　(i +1);
-            var py = a_canvas.height/2 - a_canvas.height*(v / max_v)/2;
-            points.push({"x":px,"y":py});
-        }
-        // 绘制折现
-        context.beginPath();
-        context.moveTo(points[0].x, points[0].y);
-        for(var i= 1; i< points.length; i++){
-            context.lineTo(points[i].x,points[i].y);
-        }
-        context.lineWidth = 2;
-        context.strokeStyle = "#5C6287";
-        context.stroke();
-    },false);
-})();
-(function (){
-
-    window.addEventListener("load", function(){
-        var data = [100,200,0,700,230,12,58,68,35,99,11,22,5,88,99,66,58,458,457,52,35,25,158,15,12,700,230,700,230,12,58,68,35,99,11,22,5,88,99,66,58,458,457,52,35,25,158,15,12,12,58,68,35,99,11,22,5,88,99,66,58,458,457,52,35,25,158,15,12,15,88,35,21,25,258,33,245,698,85,69,36,333];
-        // 获取上下文
-        var a_canvas = document.getElementById('can4');
-        var context = a_canvas.getContext("2d");
-        // 绘制背景
-        var gradient = context.createLinearGradient(0,0,300,135);
-        gradient.addColorStop(0,"#ffffff");
-        gradient.addColorStop(1,"#F7F7F7");
-        context.fillStyle = gradient;
-        context.fillRect(0,0,a_canvas.width,a_canvas.height);
-        // 描绘边框
-        var grid_cols = data.length + 1;
-        var grid_rows = 4;
-        var cell_height = a_canvas.height / grid_rows;
-        var cell_width = a_canvas.width / grid_cols;
-        context.lineWidth = 1;
-        context.strokeStyle = "#a0a0a0";
-        // 结束边框描绘
-        context.beginPath();
-        // 准备画横线
-        for (var col = 0; col <= grid_cols; col++) {
-            var x = col * cell_width;
-            context.moveTo(x,0);
-            context.lineTo(x,a_canvas.height);
-        }
-        // 准备画竖线
-        for(var row = 0; row <= grid_rows; row++){
-            var y = row * cell_height;
-            context.moveTo(0,y);
-            context.lineTo(a_canvas.width, y);
-        }
-        //划横线
-        context.moveTo(0,a_canvas.height/2);
-        context.lineTo(a_canvas.width,a_canvas.height/2);
-        //画竖线
-        context.moveTo(0,0);
-        context.lineTo(0,a_canvas.height);
-        context.lineWidth = 1;
-        context.strokeStyle = "#ededed";
-        context.stroke();
-        var max_v =0;
-        for(var i = 0; i<data.length; i++){
-            var d=0;
-            if(data[i]<0)
-            {dd=d-data[i];
-            }
-            else{d=data[i];};
-            if (d > max_v) { max_v =d};
-        }
-        max_vmax_v = max_v * 1.1;
-        // 将数据换算为坐标
-        var points = [];
-        for( var i=0; i < data.length; i++){
-            var v= data[i];
-            var px = cell_width *　(i +1);
-            var py = a_canvas.height/2 - a_canvas.height*(v / max_v)/2;
-            points.push({"x":px,"y":py});
-        }
-        // 绘制折现
-        context.beginPath();
-        context.moveTo(points[0].x, points[0].y);
-        for(var i= 1; i< points.length; i++){
-            context.lineTo(points[i].x,points[i].y);
-        }
-        context.lineWidth = 2;
-        context.strokeStyle = "#5C6287";
-        context.stroke();
-    },false);
-})();
-(function (){
-
-    window.addEventListener("load", function(){
-        var data = [100,200,0,700,230,12,58,68,35,99,11,22,5,88,99,66,58,458,457,52,35,25,158,15,12,700,230,700,230,12,58,68,35,99,11,22,5,88,99,66,58,458,457,52,35,25,158,15,12,12,58,68,35,99,11,22,5,88,99,66,58,458,457,52,35,25,158,15,12,15,88,35,21,25,258,33,245,698,85,69,36,333];
-        // 获取上下文
-        var a_canvas = document.getElementById('can5');
-        var context = a_canvas.getContext("2d");
-        // 绘制背景
-        var gradient = context.createLinearGradient(0,0,300,135);
-        gradient.addColorStop(0,"#ffffff");
-        gradient.addColorStop(1,"#F7F7F7");
-        context.fillStyle = gradient;
-        context.fillRect(0,0,a_canvas.width,a_canvas.height);
-        // 描绘边框
-        var grid_cols = data.length + 1;
-        var grid_rows = 4;
-        var cell_height = a_canvas.height / grid_rows;
-        var cell_width = a_canvas.width / grid_cols;
-        context.lineWidth = 1;
-        context.strokeStyle = "#a0a0a0";
-        // 结束边框描绘
-        context.beginPath();
-        // 准备画横线
-        for (var col = 0; col <= grid_cols; col++) {
-            var x = col * cell_width;
-            context.moveTo(x,0);
-            context.lineTo(x,a_canvas.height);
-        }
-        // 准备画竖线
-        for(var row = 0; row <= grid_rows; row++){
-            var y = row * cell_height;
-            context.moveTo(0,y);
-            context.lineTo(a_canvas.width, y);
-        }
-        //划横线
-        context.moveTo(0,a_canvas.height/2);
-        context.lineTo(a_canvas.width,a_canvas.height/2);
-        //画竖线
-        context.moveTo(0,0);
-        context.lineTo(0,a_canvas.height);
-        context.lineWidth = 1;
-        context.strokeStyle = "#ededed";
-        context.stroke();
-        var max_v =0;
-        for(var i = 0; i<data.length; i++){
-            var d=0;
-            if(data[i]<0)
-            {dd=d-data[i];
-            }
-            else{d=data[i];};
-            if (d > max_v) { max_v =d};
-        }
-        max_vmax_v = max_v * 1.1;
-        // 将数据换算为坐标
-        var points = [];
-        for( var i=0; i < data.length; i++){
-            var v= data[i];
-            var px = cell_width *　(i +1);
-            var py = a_canvas.height/2 - a_canvas.height*(v / max_v)/2;
-            points.push({"x":px,"y":py});
-        }
-        // 绘制折现
-        context.beginPath();
-        context.moveTo(points[0].x, points[0].y);
-        for(var i= 1; i< points.length; i++){
-            context.lineTo(points[i].x,points[i].y);
-        }
-        context.lineWidth = 2;
-        context.strokeStyle = "#5C6287";
-        context.stroke();
-    },false);
-})();
 $("header").load("header.html")
 $("footer").load("footer.html")
 $(".e_containerRight_item").each(function (index) {
